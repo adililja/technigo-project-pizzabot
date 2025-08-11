d6062486148fbe45fabd4b5f375492b5c959b4a6
Adriana Lilja <adriana.lilja@gmail.com>
2025-08-11 14:50:47 +0200

    update - step 2 finished

--- code/script.js
+++ code/script.js
@@ -1,13 +1,34 @@
 // Start here
 
 // Step 1 - Welcome and introduction
-// Your code goes here
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
+const userName = prompt("🍭 Hi, what is your name?", "write here")
+alert(
+  `Welcome to our hub, ${userName}!🏜️`
+)
+
 // Step 2 - Food choice
-// Your code goes here
+const pizza = "🍕Pizza"
+const sallad = "🥗Sallad"
+const pasta = "🍝Pasta"
+
+let foodChoice = prompt(`What would you like to eat today? \nChoose something below: \n1 - ${pizza} \n2- ${sallad} \n3 - ${pasta}`, "enter number")
+
+if (foodChoice === 1) {
+  foodChoice = pizza
+} else if (foodChoice === 2) {
+  foodChoice = sallad
+} else {
+  foodChoice = pasta
+}
+
+alert (
+  `Cool ${userName}, you chose ${foodChoice}`
+)
+
 
 // Step 3 - Subtype choice
 // Your code goes here
