52691caa4246578a42e4f4de02bad1d2eb66d72e
Adriana Lilja <adriana.lilja@gmail.com>
2025-08-12 11:06:08 +0200

    update - done with step 3

--- code/script.js
+++ code/script.js
@@ -11,27 +11,53 @@ alert(
 )
 
 // Step 2 - Food choice
-const pizza = "🍕Pizza"
-const sallad = "🥗Sallad"
-const pasta = "🍝Pasta"
+const foodChoice = prompt(`What would you like to eat today? 
+Choose something below:
+1 - Pizza 
+2 - Sallad 
+3 - Pasta`,
+"enter number")
 
-let foodChoice = prompt(`What would you like to eat today? \nChoose something below: \n1 - ${pizza} \n2- ${sallad} \n3 - ${pasta}`, "enter number")
-
-if (foodChoice === 1) {
-  foodChoice = pizza
-} else if (foodChoice === 2) {
-  foodChoice = sallad
+if (foodChoice < 2) {
+	alert(`Cool ${userName}, you chose 🍕Pizza`)
+} else if (foodChoice == 2) {
+	alert(`Cool ${userName}, you chose 🥗Sallad`)
 } else {
-  foodChoice = pasta
+	alert(`Cool ${userName}, you chose 🍝Pasta`)	
 }
 
-alert (
-  `Cool ${userName}, you chose ${foodChoice}`
-)
-
-
 // Step 3 - Subtype choice
-// Your code goes here
+let subPizza = undefined
+let subSallad = undefined
+let subPasta = undefined
+
+if (foodChoice < 2) {
+	subPizza = Number(prompt('What kind of pizza would you like? \n1 - Pepperoni \n2 - Africana \n3 - House Special', 'write here'))
+} else if (foodChoice == 2) {
+	subSallad = Number(prompt('What kind of sallad would you like? \n1 - Nicoise \n2 - Big \n3 - Ceasar', 'write here'))
+} else {
+  subPasta = Number(prompt('What kind of pasta would you like? \n1 - Alfredo \n2 - Bolognese \n3 - Anjovis', 'write here'))
+}
+
+if (subPizza < 2) {
+	alert('One Pepperoni Pizza coming up')
+} else if (subPizza == 2) {
+	alert('One Africana Pizza coming up')
+} else if (subPizza > 2) {
+	alert('One House Special Pizza coming up')
+} else if (subSallad < 2) {
+	alert('One Sallad Nicosie 4 U <3')
+} else if (subSallad == 2) {
+	alert('ONE BIG SALLAD 4 U <3')
+} else if (subSallad > 2){
+	alert('One Ceasar Sallad 4 U <3')
+} else if (subPasta < 2) {
+	alert('Pasta ALFREDO!!!!')
+} else if (subPasta == 2) {
+	alert('Pasta BoLoGnEsE!!!!')
+} else {
+	alert('Pasta with Anjovis!!!!')
+}
 
 // Step 4 - Age
 // Your code goes here
