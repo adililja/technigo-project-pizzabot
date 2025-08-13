9bb29b212682b57b22e2c5494c8df9b39731637e
Adriana Lilja <adriana.lilja@gmail.com>
2025-08-13 10:59:31 +0200

    update - streamlined code with switch statements

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 The assignment is about using JavaScript to make an order in a mock-online-pizzeria. The customer should be able to order a meal and specify it. The customer will also be able to order for a child or adult and finaly confirm their order.
 
-I followed the provided instructions and tried to search for clean code practices.
+I followed the provided Technigo instructions and tried to search for clean code practices.
 
 ## View it live
 
--- code/switch.js
+++ code/switch.js
@@ -18,12 +18,16 @@ Choose something below:
 "enter number"))
 
 let foodName = undefined
-if (foodChoice === 1) {
-    foodName = 'Pizza🍕'
-} else if (foodChoice === 2) {
-    foodName = 'Salad🥗'
-} else {
-	foodName = 'Pasta🍝'
+switch (foodChoice) {
+  case 1:
+		foodName = 'Pizza🍕'
+		break
+	case 2:
+		foodName = 'Salad🥗'
+		break
+	case 3:
+		foodName = 'Pasta🍝'
+		break
 }
 
 alert(`Cool ${userName}, you chose ${foodName}`)
@@ -32,38 +36,55 @@ alert(`Cool ${userName}, you chose ${foodName}`)
 let subChoice = undefined
 let subName = undefined
 
-if (foodChoice === 1) {
-	subChoice = Number(prompt('What kind of pizza would you like? \n1 - Pepperoni \n2 - Africana \n3 - House Special', 'write here'))
-} else if (foodChoice === 2) {
-	subChoice = Number(prompt('What kind of salad would you like? \n1 - Nicoise \n2 - Big \n3 - Ceasar', 'write here'))
-} else {
-  subChoice = Number(prompt('What kind of pasta would you like? \n1 - Alfredo \n2 - Bolognese \n3 - Anjovis', 'write here'))
-}
-
-if (foodChoice === 1) {                 //pizza menu
-	if (subChoice === 1) {
-		subName = 'Pepperoni'
-	} else if (subChoice === 2) {   
-		subName = 'Africana'
-	} else {
-		subName = 'House Special'
-	}
-} else if (foodChoice === 2) {          //Salad menu
-	if (subChoice === 1){
-		subName = 'Nicoise'
-	} else if (subChoice === 2) {
-		subName = 'BIG'
-	} else {
-		subName = 'Ceasar'
-	}
-} else {                               //pasta menu
-	if (subChoice === 1){
-		subName = 'Alfredo'
-	} else if (subChoice === 2) {
-		subName = 'BoLoGnEsE'
-	} else {
-		subName = 'Anjovis'
-	}
+switch (foodChoice) {
+	
+  // pizza
+  case 1:             
+		subChoice = Number(prompt('What kind of pizza would you like? \n1 - Pepperoni \n2 - Africana \n3 - House Special', 'write here'))
+		switch (subChoice) {
+			case 1:
+				subName = 'Pepperoni'
+				break
+			case 2:
+				subName = 'Africana'
+				break
+			case 3:
+				subName = 'House Special'
+				break
+		}
+		break
+	
+  //salad
+  case 2:                            
+		subChoice = Number(prompt('What kind of salad would you like? \n1 - Nicoise \n2 - Big \n3 - Ceasar', 'write here'))
+		switch (subChoice) {
+			case 1:
+				subName = 'Nicoise'
+				break
+			case 2:
+				subName = 'BIG'
+				break
+			case 3:
+				subName = 'Ceasar'
+				break
+		}
+		break
+	
+  //pasta
+  case 3:
+		subChoice = Number(prompt('What kind of pasta would you like? \n1 - Alfredo \n2 - Bolognese \n3 - Anjovis', 'write here'))
+		switch (subChoice) {
+			case 1:
+				subName = 'Alfredo'
+				break
+			case 2:
+				subName = 'BoLoGnEsE'
+				break
+			case 3:
+				subName = 'Anjovis'
+				break
+		}
+		break
 }
 
 alert (`One ${subName} ${foodName} coming up!!`)
@@ -78,11 +99,13 @@ if (age >= 18) {
 
 // Order confirmation
 const orderConfirmation = Number(prompt('Is this what you want to order? Type a number to confirm\n1 - yes\n2 - no'))
-
-if (orderConfirmation === 1) {
-  alert('Lovely, your meal will be coming up in a minute 🤤')
-} else if (orderConfirmation === 2) {
-  alert('It is ok to change your mind 😇')
-} else {
-  alert('Invalid choice. Please select 1 for Yes or 2 for No')
+switch (orderConfirmation) {
+	case 1:
+		alert('Lovely, your meal will be coming up in a minute 🤤')
+		break
+	case 2:
+		alert('It is ok to change your mind 😇')
+		break
+	default:
+		alert('Invalid choice. Please select 1 for Yes or 2 for No')
 }
